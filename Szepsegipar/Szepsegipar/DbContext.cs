7198b85611f50f23e4a11e0245a95b6e83443562
KricsDavid <david@kricsfalusi.hu>
2024-10-03 08:12:02 +0200

    Adatabzis

---
+++ Szepsegipar/Szepsegipar/DbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Szepsegipar
+{
+    
+
+    public class DatabaseService
+    {
+        private string connectionString = "server=localhost;database=szepsegszalon;";
+
+        public MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(connectionString);
+        }
+    }
+}
